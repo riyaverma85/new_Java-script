0e46627649bfa637b4e30dc2549706331b3d55a5
riyaverma85 <riyaverma2976@gmail.com>
2025-11-09 09:23:43 -0800

    first commit

---
+++ first.js
@@ -0,0 +1,29 @@
+
+console.log("hello my dear babuu");
+
+ let Mycollege="Sistech";
+
+console.log( Mycollege);
+
+
+
+const customer={
+    Name:"Ritika",
+    age:25,
+    city:"bhopal"
+}
+
+customer["city"]="jambu"
+customer["age"]=customer["age"]-10
+customer["Name"]="Rites Bhai"
+console.log(customer["Name"])
+console.log(customer["age"])
+console.log(customer["city"])   
+
+
+
+
+
+
+
+
