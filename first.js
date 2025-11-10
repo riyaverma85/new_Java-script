e00acd00ad43d6ea2b4b216cbe728b2990fefe95
riyaverma85 <riyaverma2976@gmail.com>
2025-11-09 16:01:40 -0800

    condition_and_prompt

--- first.js
+++ first.js
@@ -123,8 +123,28 @@
 // console.log("hello riya")
 //////////////////////////////
 
-let  num=prompt("enter a number")
-if(num%5===0){
-    console.log("divide")
+// let  num=prompt("enter a number")
+// if(num%5===0){
+//     console.log("divide")
+// }
+// else{console.log("not divide")}
+///////////////////////////////////////
+
+let score=prompt("enter your score(0-100");
+let grade;
+if(score>=90&&score<=100){
+    grade="A";
 }
-else{console.log("not divide")}
+else if(score>=70&&score<=90){
+     grade="B"
+}
+else if(score>=60&&score<=69){
+   grade="c"
+}
+else if(score>=50&&score<=59){
+    grade="D"
+}
+else if(score>=0&&score<=49){
+    grade="F"
+}
+console.log("your grade is",grade)
