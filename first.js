ae25c4e9d18497a8a871a851eae01c8d7ce346d7
riyaverma85 <riyaverma2976@gmail.com>
2026-03-14 00:42:51 -0700

    ,.,.

--- first.js
+++ first.js
@@ -32,7 +32,7 @@
 ///////////////////   + , - , / , % , *   ///////////////
 
 //  let a=5;
-// let b=2;
+//  let b=2;
 //    console.log("a+b=",a+b);
 // console.log("a-b=",a-b);
 // console.log("a*b=",a*b);
