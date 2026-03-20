ecaaaff74dfcfa85768921b0c8a868a11c3bccf3
riyaverma85 <riyaverma2976@gmail.com>
2026-03-19 23:15:18 -0700

    nkuhiu

--- first.js
+++ first.js
@@ -13,7 +13,7 @@
 //     city:"bhopal"
 // }
 // customer["city"] ="jambu"
-// customer["age"]=customer["age"]-10
+// customer["age"] =customer["age"]-10
 // customer["Name"]="Rites Bhai"
 // console.log(customer["Name"])
 // console.log(customer["age"])
