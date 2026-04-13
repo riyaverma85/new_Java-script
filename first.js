2acce63c036a6467350b4997e35702c0f822b796
riyaverma85 <riyaverma2976@gmail.com>
2026-04-12 20:59:42 -0700

    ,.,.

--- first.js
+++ first.js
@@ -202,7 +202,7 @@
 
 //////////////////////////////////////
 
-// let heroes= ["salman","shahrukh","aamir","akshay","ranbir"];
+// let heroes = ["salman","shahrukh","aamir","akshay","ranbir"];
 // for(let i= 0; i<heroes.length; i++){
 //     console.log(heroes[i])
 // }
